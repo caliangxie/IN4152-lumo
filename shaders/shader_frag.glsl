4f848a3eb90ec40135393fb9db1bb0baad903282
Ryudas <sivlesegrob9@gmail.com>
2020-06-07 19:49:08 +0200

    Component-wise implementation of Sodel operator

--- shaders/shader_frag.glsl
+++ shaders/shader_frag.glsl
@@ -44,8 +44,8 @@ void main()
 	vec4 sobel_edge_v = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);
   	vec4 sobel_edge_h = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);
 	vec4 sobel = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
-	vec3 out_vec =  sobel.rgb;
-	outColor = vec4(  out_vec, 1.0 );
+	vec3 out_vec =  vec3(length(sobel_edge_v), length(sobel_edge_h),0.0);
+	outColor = vec4( normalize(out_vec), 1.0 );
 
 
 
