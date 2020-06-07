153bcb4fbe86614c531648aeb6d403d673f5ba15
Ryudas <sivlesegrob9@gmail.com>
2020-06-07 12:21:19 +0200

    Added initial code based on praticum

---
+++ src/stb_image.cpp
@@ -0,0 +1,5 @@
+#include "disable_all_warnings.h"
+DISABLE_WARNINGS_PUSH()
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+DISABLE_WARNINGS_POP()
---
+++ src/tinyobj.cpp
@@ -0,0 +1,5 @@
+#include "disable_all_warnings.h"
+DISABLE_WARNINGS_PUSH()
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+DISABLE_WARNINGS_POP()
